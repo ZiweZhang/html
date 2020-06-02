729ac6717e974dc0020ae5e5006608068edb7535
ZiweZhang <58477461+ZiweZhang@users.noreply.github.com>
2020-06-02 21:52:11 +0200

    asd

--- php/arduino_opgevraagd.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "var.php";
-
-echo $_SESSION['briefjes'];
-
-$_SESSION['briefjes'] = "";
-?>
--- php/engels.php
+++
@@ -1,7 +0,0 @@
-<?php
-include "var.php";
-
-$_SESSION["taal"] = "Engels";
-
-header("location: ../index.php");
-?>
